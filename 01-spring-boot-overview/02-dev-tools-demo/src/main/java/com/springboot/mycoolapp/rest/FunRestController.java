cb3a82939314840c9256ac660f0b6122f08d07f0
Dustin Vetter <dustinvetter@icloud.com>
2025-12-14 12:19:36 +0100

    remove unused imports

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/springboot/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/springboot/mycoolapp/rest/FunRestController.java
@@ -2,9 +2,6 @@ package com.springboot.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 public class FunRestController {

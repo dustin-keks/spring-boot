8c23f722189cb485685a1bf2dbb139335508c931
Dustin Vetter <dustinvetter@icloud.com>
2025-12-16 20:03:00 +0100

    Setter Injection

--- dev-spring-boot/src/main/java/com/springboot/springcoredemo/rest/DemoController.java
+++ dev-spring-boot/src/main/java/com/springboot/springcoredemo/rest/DemoController.java
@@ -12,9 +12,14 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    // define a constructor for dependency injection
+    // // define a constructor for dependency injection
+    // @Autowired
+    // public DemoController(Coach theCoach) {
+    //     myCoach = theCoach;
+    // }
+
     @Autowired
-    public DemoController(Coach theCoach) {
+    public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
 

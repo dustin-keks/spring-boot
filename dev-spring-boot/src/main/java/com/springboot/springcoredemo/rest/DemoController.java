0c2a5011c1f08b6ff448c56591705bd6c68292bc
Dustin Vetter <dustinvetter@icloud.com>
2025-12-17 17:12:09 +0100

    Lazy Initialization

--- dev-spring-boot/src/main/java/com/springboot/springcoredemo/rest/DemoController.java
+++ dev-spring-boot/src/main/java/com/springboot/springcoredemo/rest/DemoController.java
@@ -1,6 +1,7 @@
 package com.springboot.springcoredemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.springcoredemo.common.Coach;
@@ -14,7 +15,8 @@ public class DemoController {
 
     // Constructor Injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 

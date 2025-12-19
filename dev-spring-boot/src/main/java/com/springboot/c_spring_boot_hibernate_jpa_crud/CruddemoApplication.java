4738d7753490b14da454e1fc3f0cc4d66db818a1
Dustin Vetter <dustinvetter@icloud.com>
2025-12-19 18:27:39 +0100

    Saving a Java Object with JPA

--- dev-spring-boot/src/main/java/com/springboot/c_spring_boot_hibernate_jpa_crud/CruddemoApplication.java
+++ dev-spring-boot/src/main/java/com/springboot/c_spring_boot_hibernate_jpa_crud/CruddemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.springboot.c_spring_boot_hibernate_jpa_crud.dao.StudentDAO;
+import com.springboot.c_spring_boot_hibernate_jpa_crud.entity.Student;
+
 @SpringBootApplication
 public class CruddemoApplication {
     public static void main(String[] args) {
@@ -12,9 +15,22 @@ public class CruddemoApplication {
 	}
 
     @Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating a new student object...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@doe.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }

ab77650a8b917126c574462f17b532ad0f28a3db
Dustin Vetter <dustinvetter@icloud.com>
2025-12-19 18:17:50 +0100

    Saving a Java Object with JPA

---
+++ dev-spring-boot/src/main/java/com/springboot/c_spring_boot_hibernate_jpa_crud/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.springboot.c_spring_boot_hibernate_jpa_crud.dao;
+
+import com.springboot.c_spring_boot_hibernate_jpa_crud.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
---
+++ dev-spring-boot/src/main/java/com/springboot/c_spring_boot_hibernate_jpa_crud/dao/StudentDAOImpl.java
@@ -0,0 +1,28 @@
+package com.springboot.c_spring_boot_hibernate_jpa_crud.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.c_spring_boot_hibernate_jpa_crud.entity.Student;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}

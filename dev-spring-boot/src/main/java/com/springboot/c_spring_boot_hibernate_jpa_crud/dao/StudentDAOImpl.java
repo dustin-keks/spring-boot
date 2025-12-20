3301fece8518e7f6297355a0551fadfae16e8d60
Dustin Vetter <dustinvetter@icloud.com>
2025-12-20 21:14:15 +0100

    Updating Objects with JPA

--- dev-spring-boot/src/main/java/com/springboot/c_spring_boot_hibernate_jpa_crud/dao/StudentDAOImpl.java
+++ dev-spring-boot/src/main/java/com/springboot/c_spring_boot_hibernate_jpa_crud/dao/StudentDAOImpl.java
@@ -57,4 +57,11 @@ public class StudentDAOImpl implements StudentDAO{
         // return query results
         return theQuery.getResultList();
     }
+
+    // implement update mthod
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
 }

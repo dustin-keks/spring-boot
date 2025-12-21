5b37f245b08586ebb91fbae7f8c89c314bf3ec5b
Dustin Vetter <dustinvetter@icloud.com>
2025-12-21 18:36:55 +0100

    Spring Boot REST Global Exception Handling

--- dev-spring-boot/src/main/java/com/springboot/d_spring_boot_rest_crud/rest/StudentRestController.java
+++ dev-spring-boot/src/main/java/com/springboot/d_spring_boot_rest_crud/rest/StudentRestController.java
@@ -38,7 +38,7 @@ public class StudentRestController {
     }
     
     // define endpoint for "/students/{studentId}" - return student at index
-    @GetMapping("/student/{studentId}")
+    @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
         // just index into the list ... keep it simple now
         // check the studentId against list size
@@ -47,33 +47,5 @@ public class StudentRestController {
         }
 
         return theStudents.get(studentId);
-    }
-
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another excpetion handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }

2dc0971e3c85a21ba77e8d87f12251ab3dd8ac58
Dustin Vetter <dustinvetter@icloud.com>
2025-12-21 18:37:53 +0100

    remove unused imports

--- dev-spring-boot/src/main/java/com/springboot/d_spring_boot_rest_crud/rest/StudentRestController.java
+++ dev-spring-boot/src/main/java/com/springboot/d_spring_boot_rest_crud/rest/StudentRestController.java
@@ -10,9 +10,6 @@ import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 

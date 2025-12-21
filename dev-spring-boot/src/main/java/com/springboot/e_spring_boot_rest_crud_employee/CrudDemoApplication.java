ac3cca7969fde57dd5c32eaed03966464e790238
Dustin Vetter <dustinvetter@icloud.com>
2025-12-21 21:29:30 +0100

    Create new package for REST DAO

---
+++ dev-spring-boot/src/main/java/com/springboot/e_spring_boot_rest_crud_employee/CrudDemoApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.e_spring_boot_rest_crud_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+}

160904657b0d4b897f4aae1b112f71cf04cd00d4
Dustin Vetter <dustinvetter@icloud.com>
2025-12-26 22:35:52 +0100

    Spring Boot REST: Update Employee

--- dev-spring-boot/src/main/java/com/springboot/e_spring_boot_rest_crud_employee/rest/EmployeeRestController.java
+++ dev-spring-boot/src/main/java/com/springboot/e_spring_boot_rest_crud_employee/rest/EmployeeRestController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/api")
@@ -48,6 +50,13 @@ public class EmployeeRestController {
         // also just in case they pass an id in JSON ... set id to 0
         // this is to force a save of new item ... instead of update
         theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    // add mapping for PUT /employees - update exisiting employee
+    @PutMapping("/employees")
+    public Employee updatEmployee(@RequestBody Employee theEmployee) {
         Employee dbEmployee = employeeService.save(theEmployee);
         return dbEmployee;
     }

b23f54b4e40eb31e8688d183335d9a16b039b80f
Dustin Vetter <dustinvetter@icloud.com>
2025-12-28 12:40:16 +0100

    Spring Boot REST: Spring Data REST Configs and Sorting

--- dev-spring-boot/src/main/java/com/springboot/g_spring_boot_rest_crud_employee_spring_data_rest/dao/EmployeeRepository.java
+++ dev-spring-boot/src/main/java/com/springboot/g_spring_boot_rest_crud_employee_spring_data_rest/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.g_spring_boot_rest_crud_employee_spring_data_rest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.g_spring_boot_rest_crud_employee_spring_data_rest.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it ... no need to write any code!
 }

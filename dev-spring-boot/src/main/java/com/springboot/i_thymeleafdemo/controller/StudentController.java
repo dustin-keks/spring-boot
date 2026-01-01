966639221dfa54fcddb0babab3d47b7aca07a79f
Dustin Vetter <dustinvetter@icloud.com>
2026-01-01 21:07:45 +0100

    Spring Boot - Spring MVC Form Data Binding - Check Boxes

--- dev-spring-boot/src/main/java/com/springboot/i_thymeleafdemo/controller/StudentController.java
+++ dev-spring-boot/src/main/java/com/springboot/i_thymeleafdemo/controller/StudentController.java
@@ -19,6 +19,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         // create a student object
@@ -32,6 +35,9 @@ public class StudentController {
 
         // add the list of languages to the model
         theModel.addAttribute("languages", languages);
+
+        // add the list of operating systems to the model
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }

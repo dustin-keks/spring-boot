d0d5faafc9034ae0eec27eeedc781ea49dd94dc8
Dustin Vetter <dustinvetter@icloud.com>
2026-01-02 11:58:35 +0100

    Spring Boot - Spring MVC Validation - Validate a Number Range

--- dev-spring-boot/src/main/java/com/springboot/j_validationdemo/Customer.java
+++ dev-spring-boot/src/main/java/com/springboot/j_validationdemo/Customer.java
@@ -1,13 +1,20 @@
 package com.springboot.j_validationdemo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
+
     @NotNull(message="is required")
     @Size(min=1, message="is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -20,5 +27,11 @@ public class Customer {
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+    public int getFreePasses() {
+        return freePasses;
+    }
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }

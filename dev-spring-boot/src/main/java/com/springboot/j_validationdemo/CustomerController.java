e6819a18c89c394dfdef6dff733f749b163629e0
Dustin Vetter <dustinvetter@icloud.com>
2026-01-02 12:38:43 +0100

    Spring Boot - Spring MVC Validation - Debugging Tips for Custom Error Names

--- dev-spring-boot/src/main/java/com/springboot/j_validationdemo/CustomerController.java
+++ dev-spring-boot/src/main/java/com/springboot/j_validationdemo/CustomerController.java
@@ -33,6 +33,7 @@ public class CustomerController {
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
         System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+        System.out.println("Bindung results: " + theBindingResult.toString());
         
         if (theBindingResult.hasErrors()) {
             return "customer-form";

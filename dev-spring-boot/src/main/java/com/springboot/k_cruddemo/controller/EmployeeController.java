c801c8ea721dd4fa325857be422f7dddacecdb27
Dustin Vetter <dustinvetter@icloud.com>
2026-01-02 22:17:19 +0100

    CRUD Database Project - Update Employee

--- dev-spring-boot/src/main/java/com/springboot/k_cruddemo/controller/EmployeeController.java
+++ dev-spring-boot/src/main/java/com/springboot/k_cruddemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springboot.k_cruddemo.entity.Employee;
 import com.springboot.k_cruddemo.service.EmployeeService;
@@ -42,6 +43,18 @@ public class EmployeeController {
 
         theModel.addAttribute("employee", theEmployee);
         
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to our form
         return "employees/employee-form";
     }
 

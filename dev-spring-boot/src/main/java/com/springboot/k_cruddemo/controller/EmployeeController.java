b48a151b35db63cc01d37dba7f6d9d850aa8c0c9
Dustin Vetter <dustinvetter@icloud.com>
2026-01-02 20:54:10 +0100

    CRUD Database Project - Get Employees

---
+++ dev-spring-boot/src/main/java/com/springboot/k_cruddemo/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.springboot.k_cruddemo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springboot.k_cruddemo.entity.Employee;
+import com.springboot.k_cruddemo.service.EmployeeService;
+
+
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService thEmployeeService) {
+        employeeService = thEmployeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        // get the employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+}

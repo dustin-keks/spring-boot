92c1048f4e0c1c49d11f1984d2e093270546356d
Dustin Vetter <dustinvetter@icloud.com>
2026-01-02 22:00:12 +0100

    CRUD Database Project - Add Employee

--- dev-spring-boot/src/main/java/com/springboot/k_cruddemo/dao/EmployeeRepository.java
+++ dev-spring-boot/src/main/java/com/springboot/k_cruddemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.springboot.k_cruddemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.k_cruddemo.entity.Employee;
@@ -8,4 +10,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
 
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
 }

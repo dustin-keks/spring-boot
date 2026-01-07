8ce9f3c8fd77b87d30434f8873ee392786cafe71
Dustin Vetter <dustinvetter@icloud.com>
2026-01-07 18:36:42 +0100

    @OneToOne Mapping - Bi-Directional - Only Delete Instructor Details

--- dev-spring-boot/src/main/java/com/springboot/n_cruddemo_onetoone_bi/entity/InstructorDetail.java
+++ dev-spring-boot/src/main/java/com/springboot/n_cruddemo_onetoone_bi/entity/InstructorDetail.java
@@ -29,7 +29,7 @@ public class InstructorDetail {
 
 
     // add @OneToOne annotation
-    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private Instructor instructor;
 
     

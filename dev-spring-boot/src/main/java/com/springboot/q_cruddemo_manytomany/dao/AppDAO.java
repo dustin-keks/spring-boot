acd8f37ea11127cbe6027590236bfb0ce6a93333
Dustin Vetter <dustinvetter@icloud.com>
2026-01-09 15:38:00 +0100

    duplicate project

---
+++ dev-spring-boot/src/main/java/com/springboot/q_cruddemo_manytomany/dao/AppDAO.java
@@ -0,0 +1,35 @@
+package com.springboot.q_cruddemo_manytomany.dao;
+
+import java.util.List;
+
+import com.springboot.q_cruddemo_manytomany.entity.Course;
+import com.springboot.q_cruddemo_manytomany.entity.Instructor;
+import com.springboot.q_cruddemo_manytomany.entity.InstructorDetail;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+}

a436dd7100fb598486c4964b33f5d52bcdb81f95
Dustin Vetter <dustinvetter@icloud.com>
2026-01-10 19:07:41 +0100

    AOP - Pointcut Expressions - Match only AccountDAO addAccount

--- dev-spring-boot/src/main/java/com/springboot/r_aopdemo/aspect/MyDemoLoggingAspect.java
+++ dev-spring-boot/src/main/java/com/springboot/r_aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,7 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.springboot.r_aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("========== Executing @Before advice on method");
     }

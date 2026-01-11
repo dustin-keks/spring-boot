45c935e472e4cb093a69c17e7e4718e6072fe3fc
Dustin Vetter <dustinvetter@icloud.com>
2026-01-11 22:37:37 +0100

    AOP: @After Advice

--- dev-spring-boot/src/main/java/com/springboot/s_aopdemo_pointcut_declarations/aspect/MyDemoLoggingAspect.java
+++ dev-spring-boot/src/main/java/com/springboot/s_aopdemo_pointcut_declarations/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.springboot.s_aopdemo_pointcut_declarations.aspect;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
@@ -17,6 +18,15 @@ import com.springboot.s_aopdemo_pointcut_declarations.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+    @After("execution(* com.springboot.s_aopdemo_pointcut_declarations.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n===== Executing @After (finally) on method: " + method + " =====");
+    }
+
+
+
     @AfterThrowing(
             pointcut = "execution(* com.springboot.s_aopdemo_pointcut_declarations.dao.AccountDAO.findAccounts(..))",
             throwing = "theExc")
@@ -29,7 +39,7 @@ public class MyDemoLoggingAspect {
         System.out.println("\n===== The exception is: " + theExc + " =====");
     }
 
-    
+
 
     @AfterReturning(
             pointcut = "execution(* com.springboot.s_aopdemo_pointcut_declarations.dao.AccountDAO.findAccounts(..))",

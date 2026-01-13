7bbc79390b035b16998d58870737d33f49e58aff
Dustin Vetter <dustinvetter@icloud.com>
2026-01-13 21:50:17 +0100

    AOP: Integrating AOP with Spring MVC CRUD App

--- dev-spring-boot/src/main/java/com/springboot/t_thymeleafdemo/aspect/DemoLoggingAspect.java
+++ dev-spring-boot/src/main/java/com/springboot/t_thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.springboot.t_thymeleafdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -44,5 +45,16 @@ public class DemoLoggingAspect {
         for (Object tempArg : args) {
             myLogger.info("===== argument: " + tempArg + " =====");
         }
+    }
+
+    // add @AfterReturning advice
+    @AfterReturning(pointcut = "forAppFlow()", returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("===== in @AfterReturning: calling method: " + theMethod + " =====");
+
+        // display data returned
+        myLogger.info("===== result: " + theResult + " =====");
     }
 }
